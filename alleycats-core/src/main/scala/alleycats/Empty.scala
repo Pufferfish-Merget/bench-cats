2c1a99afe50a2e6d8ed3e4135bfaaadc7a9c1e70
Daniel Esik <esik.danny@gmail.com>
2025-03-30 10:19:34 +0300

    Merge pull request #4733 from danicheg/empty-option-instance
    Add the `Empty[Option[A]]` instance in alleycats

--- alleycats-core/src/main/scala/alleycats/Empty.scala
+++ alleycats-core/src/main/scala/alleycats/Empty.scala
@@ -79,7 +79,10 @@ object Empty extends EmptyInstances0 {
 
 }
 
-private[alleycats] trait EmptyInstances0 extends compat.IterableEmptyInstance with EmptyInstances1
+private[alleycats] trait EmptyInstances0 extends compat.IterableEmptyInstance with EmptyInstances1 {
+  private[this] val emptyOptionSingleton: Empty[Option[Nothing]] = Empty(None)
+  implicit def alleycatsEmptyForOption[A]: Empty[Option[A]] = emptyOptionSingleton.asInstanceOf[Empty[Option[A]]]
+}
 
 private[alleycats] trait EmptyInstances1 {
   // If Monoid extended Empty then this could be an exported subclass instance provided by Monoid
